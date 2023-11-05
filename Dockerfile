8137aa3d6e1646118a4df17bc72638e203dcdea3
pranav142 <pknadimp@gmail.com>
2023-11-04 18:35:52 -0700

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.9-slim
+
+WORKDIR /app
+
+COPY requirements.txt .
+RUN pip install --upgrade pip
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY ./src .
+COPY .env .env
+
+ENV FLASK_APP=main.py
+ENV FLASK_RUN_HOST=0.0.0.0
+ENV FLASK_RUN_PORT=6000
+
+EXPOSE 6000
+
+CMD ["flask", "run"]
---
+++ src/main.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, jsonify
+import mysql.connector
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+import os
+import requests
+
+URL = "http://3.84.46.162:5000"
+app = Flask(__name__)
+
+def update_streak(user_id: int, url: str):
+    endpoint = f'/upgrade/{user_id}'
+    full_url = url + endpoint
+    current_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    payload = {
+        'timestamp': current_timestamp
+    }
+    try:
+        response = requests.post(full_url, json=payload)
+        if response.ok:
+            return response.json()
+        else:
+            response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f'An error occurred: {e}')
+        return None
+
+def dummy_3rd_party_api():
+    return True
+
+@app.route('/payment/<int:user_id>', methods=['POST'])
+def payment(user_id):
+    payment_successful = dummy_3rd_party_api()
+    
+    if not payment_successful:
+        return jsonify({'error': 'Payment was unsuccessful'}), 400
+
+    streak_update_response = update_streak(user_id, URL)
+    
+    if streak_update_response:
+        return jsonify({'msg': 'Payment successfully proccessed'}), 200
+    else:
+        return jsonify({'error': 'Failed to update streak'}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
